647048c5eebe5a51b17d9f083a5d4a549c444aab
jegapriya <jegapriyab1996@gmail.com>
2024-01-04 17:05:52 +0530

    samplebranch files

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class Sample {
+	
+	private void Testmethod() {
+		// TODO Auto-generated method stub
+System.out.println("Testmethod1");
+	}
+
+}
--- src/test/java/org/test/Samplecode.java
+++ src/test/java/org/test/Samplecode.java
@@ -1,5 +1,10 @@
 package org.test;
 
 public class Samplecode {
+	
+	private void method1() {
+		// TODO Auto-generated method stub
+System.out.println("Test01");
+	}
 
 }
