8d07cb201e9f8a45b3b64169526b9f1262cf825c
jegapriya <jegapriyab1996@gmail.com>
2024-01-07 11:43:44 +0530

    Sample code

--- src/test/java/org/test/Samplecode.java
+++ src/test/java/org/test/Samplecode.java
@@ -5,9 +5,8 @@ import org.testng.annotations.Test;
 public class Samplecode {
 	@Test
 	private void method1() {
-		// TODO Auto-generated method stub
-System.out.println("Test01");
-System.out.println("Test02");
+System.out.println("Test1");
+System.out.println("Test2");
 	}
 
 }
